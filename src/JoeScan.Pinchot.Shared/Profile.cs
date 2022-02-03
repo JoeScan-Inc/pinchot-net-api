8cd4ed2295a580f1519410b86c5c7d6f996c65db
aaronhudon-ts <49655238+aaronhudon-ts@users.noreply.github.com>
2022-02-03 14:10:49 -0800

    Define an interface for the Profile class
    Sealing the Profile class makes it impossible for end users to mock the joe scan scan structures.

--- src/JoeScan.Pinchot.Shared/Profile.cs
+++ src/JoeScan.Pinchot.Shared/Profile.cs
@@ -1,4 +1,4 @@
-﻿// Copyright(c) JoeScan Inc. All Rights Reserved.
+// Copyright(c) JoeScan Inc. All Rights Reserved.
 //
 // Licensed under the BSD 3 Clause License. See LICENSE.txt in the project
 // root for license information.
@@ -13,19 +13,99 @@ namespace JoeScan.Pinchot
     /// <summary>
     /// Profile data from a scan head.
     /// </summary>
+    public interface IProfile
+    {
+        /// <summary>
+        /// Gets the ID of the <see cref="ScanHead"/> the <see cref="Profile"/> originates from.
+        /// </summary>
+        /// <value>The ID of the <see cref="ScanHead"/> the <see cref="Profile"/> originates from.</value>
+        uint ScanHeadID { get; }
+
+        /// <summary>
+        /// Gets the <see cref="Camera"/> the <see cref="Profile"/> originates from.
+        /// </summary>
+        /// <value>The <see cref="Camera"/> the <see cref="Profile"/> originates from.</value>
+        Camera Camera { get; }
+
+        /// <summary>
+        /// Gets the <see cref="Laser"/> used to generate the <see cref="Profile"/>.
+        /// </summary>
+        /// <value>The <see cref="Laser"/> used to generate the <see cref="Profile"/>.</value>
+        Laser Laser { get; }
+
+        /// <summary>
+        /// Gets the time of the scan head in nanoseconds when the <see cref="Profile"/> was generated.
+        /// </summary>
+        /// <value>The time of the scan head in nanoseconds when the <see cref="Profile"/> was generated.</value>
+        long Timestamp { get; }
+
+        /// <summary>
+        /// Gets the encoder positions when the <see cref="Profile"/> was generated.
+        /// </summary>
+        /// <value>A <see cref="IDictionary{TKey,TValue}"/> of encoder positions when the <see cref="Profile"/> was generated.</value>
+        IDictionary<Encoder, long> EncoderValues { get; }
+
+        /// <summary>
+        /// Gets the laser on time in microseconds used to generate the <see cref="Profile"/>.
+        /// </summary>
+        /// <value>The laser on time in microseconds used to generate the <see cref="Profile"/>.</value>
+        double LaserOnTime { get; }
+
+        /// <summary>
+        /// Gets the <see cref="Point2D"/> data for the <see cref="Profile"/>, including invalid points.
+        /// </summary>
+        /// <value>A <see cref="Span{T}"/> of <see cref="Point2D"/> data for the <see cref="Profile"/>.</value>
+        Span<Point2D> RawPoints { get; }
+
+        /// <summary>
+        /// Gets the number of valid <see cref="Point2D"/>s in <see cref="RawPoints"/>.
+        /// </summary>
+        /// <value>The number of valid <see cref="Point2D"/>s in <see cref="RawPoints"/>.</value>
+        int ValidPointCount { get; }
+
+        /// <summary>
+        /// Gets the <see cref="DataFormat"/> of the <see cref="Profile"/>.
+        /// </summary>
+        /// <value>The <see cref="DataFormat"/> of the <see cref="Profile"/>.</value>
+        DataFormat DataFormat { get; }
+
+        /// <summary>
+        /// Gets the valid <see cref="Point2D"/>s in the <see cref="Profile"/>.
+        /// </summary>
+        /// <returns>A <see cref="IEnumerable{Point2D}"/> of the valid <see cref="Point2D"/>s in the <see cref="Profile"/>.</returns>
+        IEnumerable<Point2D> GetValidXYPoints();
+
+        /// <summary>
+        /// Gets the valid <see cref="Point2D"/>s in the <see cref="Profile"/>.
+        /// </summary>
+        /// <param name="validPoints">A <see cref="Span{T}"/> of <see cref="Point2D"/> that is the
+        /// storage location for the valid <see cref="Point2D"/>s. Must be of length greater than
+        /// or equal to <see cref="ValidPointCount"/>.</param>
+        void GetValidXYPoints(Span<Point2D> validPoints);
+
+        /// <summary>
+        /// <see cref="Profile"/> implements <see cref="ICloneable"/>.
+        /// </summary>
+        /// <returns>A shallow copy of the <see cref="Profile"/> object.</returns>
+        object Clone();
+    }
+
+    /// <summary>
+    /// Profile data from a scan head.
+    /// </summary>
     /// <remarks>
     /// The <see cref="Profile"/> class provides properties and methods for accessing the information
     /// contained in a profile received from a scan head. The properties include the raw <see cref="Point2D"/> data,
     /// timestamp, encoder values, and other properties. Methods for retrieving only valid <see cref="Point2D"/>
     /// data are also provided.
     /// </remarks>
-    public class Profile : ICloneable
+    public class Profile : IProfile, ICloneable
     {
         #region Private Fields
 
         private const short ProfileMagic = 0xCBD;
 
-        #endregion
+        #endregion Private Fields
 
         #region Public Properties
 
@@ -83,7 +163,7 @@ namespace JoeScan.Pinchot
         /// <value>The <see cref="DataFormat"/> of the <see cref="Profile"/>.</value>
         public DataFormat DataFormat => (DataFormat)AllDataFormat;
 
-        #endregion
+        #endregion Public Properties
 
         #region Internal Properies
 
@@ -106,7 +186,7 @@ namespace JoeScan.Pinchot
         /// <value>The camera exposure time in microseconds used to generate the <see cref="Profile"/>.</value>
         internal double ExposureTime { get; set; }
 
-        #endregion
+        #endregion Internal Properies
 
         #region Lifecycle
 
@@ -114,7 +194,7 @@ namespace JoeScan.Pinchot
         {
         }
 
-        #endregion
+        #endregion Lifecycle
 
         #region Public Methods
 
@@ -166,7 +246,7 @@ namespace JoeScan.Pinchot
             return MemberwiseClone();
         }
 
-        #endregion
+        #endregion Public Methods
 
         #region Internal Methods
 
@@ -279,6 +359,6 @@ namespace JoeScan.Pinchot
             }
         }
 
-        #endregion
+        #endregion Internal Methods
     }
 }
